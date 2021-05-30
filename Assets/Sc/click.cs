c915b542949d506b65999de26df86e84bcb33ecb
taesoung <rkdxotjd93@naver.com>
2021-05-30 20:14:22 +0900

    first commit

---
+++ Assets/Sc/click.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click : MonoBehaviour
+{
+    public GameObject eventS;
+    public GameObject Effect;
+    private Vector3 mousePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnMouseUp()
+    {
+
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        eventS.GetComponent<stat>().clickMonster();
+         Destroy(Instantiate(Effect, new Vector3(mousePosition.x, mousePosition.y,-2.0f), Quaternion.identity), 0.8f);
+    }
+}
